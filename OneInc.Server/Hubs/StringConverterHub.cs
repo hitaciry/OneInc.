1f7be907f3f20e74b871a001289fb9c10168bb41
Andrei_Shitikov <shitikov@soramitsu.co.jp>
2023-12-25 17:47:55 +0100

    update SignalR hub for messanging with delay

--- OneInc.Server/Hubs/StringConverterHub.cs
+++ OneInc.Server/Hubs/StringConverterHub.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using OneInc.Server.Services;
+using System.Threading;
 
 namespace OneInc.Server.Hubs
 {
     public class StringConverterHub: Hub
     {
-        public async Task ConvertChar(string input)
+        private readonly IDelayService delayService;
+
+        public StringConverterHub(IDelayService delayService)
         {
-            awat
+            this.delayService = delayService;
+        }
+        public async Task ConvertToBase64(string input, CancellationToken token, [FromServices] IStringConvertService<string> stringConvertService)
+        {
+            var base64String = stringConvertService.Convert(input);
+            foreach (var charecter in base64String)
+            {
+                token.ThrowIfCancellationRequested();
+                await delayService.Delay(token);
+                await Clients.Caller.SendCoreAsync("Response", new object[] { charecter }, token);
+            }
         }
     }
 }
