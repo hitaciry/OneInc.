e7e1e92d69e4058f724e3418b48d74fea66ce821
Andrei_Shitikov <shitikov@soramitsu.co.jp>
2023-12-26 18:38:22 +0100

    fix hub dependences, add complitation message

--- OneInc.Server/Hubs/StringConverterHub.cs
+++ OneInc.Server/Hubs/StringConverterHub.cs
@@ -1,27 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using OneInc.Server.Services;
-using System.Threading;
 
 namespace OneInc.Server.Hubs
 {
     public class StringConverterHub: Hub
     {
         private readonly IDelayService delayService;
+        private readonly IStringConvertService<string> stringConvertService;
 
-        public StringConverterHub(IDelayService delayService)
+        public StringConverterHub(IDelayService delayService, IStringConvertService<string> stringConvertService)
         {
             this.delayService = delayService;
+            this.stringConvertService = stringConvertService;
         }
-        public async Task ConvertToBase64(string input, CancellationToken token, [FromServices] IStringConvertService<string> stringConvertService)
+        public async Task ConvertToBase64(string input)
         {
+            var token = Context.ConnectionAborted;
             var base64String = stringConvertService.Convert(input);
             foreach (var charecter in base64String)
             {
-                token.ThrowIfCancellationRequested();
                 await delayService.Delay(token);
-                await Clients.Caller.SendCoreAsync("Response", new object[] { charecter }, token);
+                await Clients.Caller.SendAsync("ReceiveCharacter", charecter.ToString(), token);
             }
+            await Clients.Caller.SendAsync("EncodingComplite", token);
         }
     }
 }
--- OneInc.Server/Program.cs
+++ OneInc.Server/Program.cs
@@ -2,24 +2,47 @@ using OneInc.Server.Hubs;
 using OneInc.Server.Model;
 using OneInc.Server.Services;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<DelayRange>(builder.Configuration.GetSection(nameof(DelayRange)));
+var UIOrigin = builder.Configuration.GetValue<string>("UIOrigin") ?? "https://localhost:5173";
+// Add services to the container.
+builder.Services.AddSignalR();
 builder.Services.AddTransient<IDelayService, DelayService>();
 builder.Services.AddTransient<IStringConvertService<string>, StringConvertToBase64Service>();
-builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            //TODO: set origin from configuration
+            policy.WithOrigins(UIOrigin)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-app.UseAuthorization();
-
-app.MapFallbackToFile("/index.html");
-
-app.MapHub<StringConverterHub>("/stringConverterHub");
+app.UseRouting();
+app.UseCors();
+app.MapHub<StringConverterHub>("/hub");
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapHub<StringConverterHub>("/hub");
+//});
 
 app.Run();
